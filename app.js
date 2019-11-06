f774b32db594bcb0004cda64fdf2c23b0ca3a2cb
Yazon Suleiman <yazonsuleiman@yazons-mbp.attlocal.net>
2019-11-06 00:04:30 -0800

    test

--- app.js
+++ app.js
@@ -1,9 +1,3 @@
-
-// json is a type of data where object is listed in an array
-// ajax is just async, javascript, json
-// xmlhttpreqeuest sort by alphabets
-
-
 var xhttp = new XMLHttpRequest();
 
 xhttp.onreadystatechange = function()
@@ -11,19 +5,19 @@ xhttp.onreadystatechange = function()
     if (this.readyState == 4 && this.status == 200) 
         {
 
-            var myData = JSON.parse(this.responseText) // ************ JSON.Parse is used to convert text into javascript object or array
+            var myData = JSON.parse(this.responseText) 
             console.log(myData.length)
-            // create sorted data
+          
             var sorted = []
-            console.log(typeof myData) // use this to see what type of stuff
+            console.log(typeof myData) 
             for (i=0; i< myData.length; i++)
                 {
                     console.log(myData[i].email);
-                    sorted.push(myData[i].email + '<br>') //.push is used to add elements to the end of an array, this line adds stuff to sorted array by getting data from myData
-                    // grab email of first object
+                    sorted.push(myData[i].email + '<br>') 
+                   
                 }
                 sorted = sorted.sort()
-            document.getElementById("output1").innerHTML = sorted.join(' '); // replace myData with sortedData and .join is used to customize how data is dislayed, in this case every data is seperated by a space 
+            document.getElementById("output1").innerHTML = sorted.join(' '); 
        
         }
 };
@@ -34,7 +28,7 @@ xhttp.send();
 
 
 
-// fetch username and sort by length, this is the promise syntax
+
 fetch  ("https://jsonplaceholder.typicode.com/users")
 .then  ( response => response.json())                   
 .then (data => {                                      
@@ -52,15 +46,11 @@ fetch  ("https://jsonplaceholder.typicode.com/users")
 
 
 
-//fetch  ("https://jsonplaceholder.typicode.com/users")
-//.then  ( sortLength (response)  )
-//.catch ( error => console.log('There was an error:' , error) )
-
 
 function sortLength (ourData){
 var needSort = []
 var LOL = JSON.parse(ourData)
-// console.log(LOL + "HALLO")
+
 
 for (i=0; i< LOL.length; i++)
                  {
